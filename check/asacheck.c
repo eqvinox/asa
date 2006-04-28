33e817a3370690530f64a7e52806e224c291fa2b
David Lamparter <equinox@diac24.net>
2006-04-28 13:10:29 +0200

    cleaning: integrate libraries into one; delete deprecated files

--- check/asacheck.c
+++ check/asacheck.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  ****************************************************************************/
 
+#define ASA_BUILD_RENDERER 1
+
 #include "common.h"
 #include "ssa.h"
 #include "acconf.h"
