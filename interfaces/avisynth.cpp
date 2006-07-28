51a7f97239d9bf4535b2ff07c07936163c5f969c
David Lamparter <equinox@diac24.net>
2006-07-28 02:30:52 +0200

    reorder includes (miscompiles otherwise)

--- interfaces/avisynth.cpp
+++ interfaces/avisynth.cpp
@@ -18,11 +18,11 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  ****************************************************************************/
 
-#include "common.h"
-
 #include <windows.h>
 #include "avisynth.h"
 #include <string.h>
+
+#include "common.h"
 
 #include "asa.h"
 
