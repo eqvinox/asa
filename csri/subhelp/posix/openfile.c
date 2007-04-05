3e2c26d9482af9aae51d951f773748c9d9863941
David Lamparter <equinox@diac24.net>
2007-04-05 19:06:48 +0200

    CSRI rev 33: more acconf.h references

--- csri/subhelp/posix/openfile.c
+++ csri/subhelp/posix/openfile.c
@@ -18,7 +18,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  ****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
 #include "acconf.h"
+#endif
 
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
