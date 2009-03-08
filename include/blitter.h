c1290d16f31802c88b8a3cbe62be6da4b9100cc6
David Lamparter <equinox@diac24.net>
2009-03-08 03:28:45 +0100

    relicense as BSD

--- include/blitter.h
+++ include/blitter.h
@@ -3,19 +3,18 @@
  *****************************************************************************
  * Copyright (C) 2004, 2005, 2006, 2007  David Lamparter
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * 1. Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the disclaimer
+ *    contained in the COPYING file.
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the disclaimer
+ *    contained in the COPYING file in the documentation and/or
+ *    other materials provided with the distribution.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  ****************************************************************************/
 
 #ifndef _BLITTER_H
